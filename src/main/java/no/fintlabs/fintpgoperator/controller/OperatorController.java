7b613a4a4e10548708b377b3bdd7e5c81fc0951f
vetlewj <vetlewj@gmail.com>
2022-09-08 08:56:51 +0200

    FL-772 Roles and privileges

--- src/main/java/no/fintlabs/fintpgoperator/controller/OperatorController.java
+++ src/main/java/no/fintlabs/fintpgoperator/controller/OperatorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class OperatorController {
 
@@ -30,7 +31,25 @@ public class OperatorController {
     }
 
     @PostMapping("/api/createDbUser")
-    public String createDbUser(String userName, String password) {
-        return dataAccessService.createDbUser(userName, password);
+    public String createDbUser(String username, String password) {
+        return dataAccessService.createDbUser(username, password);
+    }
+
+    @PostMapping("/api/grantPrivilegeToRole")
+    public String grantPrivilegeToRole(String schemaName, String roleName, String privilege) {
+        return dataAccessService.grantPrivilegeToRole(schemaName, roleName, privilege);
+    }
+
+    @PostMapping("/api/createRole")
+    public String createRole(String roleName, String password) {
+        return dataAccessService.createRole(roleName, password);
+    }
+    @PostMapping("/api/checkGrants")
+    public String checkGrants(String schemaName, String roleName) {
+        return dataAccessService.checkRolePrivilegesOnSchema(schemaName, roleName);
+    }
+    @PostMapping("/api/createSchemaUserAndRole")
+    public String createSchemaUserAndRole(String schemaName, String username, String password, String roleName) {
+        return dataAccessService.createSchemaUserAndRole(schemaName, username, password, roleName);
     }
 }
--- src/main/java/no/fintlabs/fintpgoperator/service/PostgreSqlDataAccessService.java
+++ src/main/java/no/fintlabs/fintpgoperator/service/PostgreSqlDataAccessService.java
@@ -14,6 +14,22 @@ public class PostgreSqlDataAccessService {
     private JdbcTemplate jdbcTemplate;
     private Logger logger;
 
+    public enum Privilege {
+        SELECT,
+        INSERT,
+        UPDATE,
+        DELETE,
+        TRUNCATE,
+        REFERENCES,
+        TRIGGER,
+        CREATE,
+        CONNECT,
+        TEMPORARY,
+        EXECUTE,
+        USAGE,
+        ALL
+    }
+
     public PostgreSqlDataAccessService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
         this.logger = Logger.getLogger(PostgreSqlDataAccessService.class.getName());
@@ -31,26 +47,92 @@ public class PostgreSqlDataAccessService {
     }
 
     public String createSchema(String schemaName) {
-        String sql = "CREATE SCHEMA " + schemaName;
+        String sqlCreateSchema = "CREATE SCHEMA IF NOT EXISTS " + schemaName;
         try {
-            jdbcTemplate.execute(sql);
-            logger.log(Level.INFO, "Schema created");
-            return "Schema created";
+            jdbcTemplate.execute(sqlCreateSchema);
+            logger.log(Level.INFO, "Schema " + schemaName + " created");
+            return "Schema " + schemaName + " created";
         } catch (Exception e) {
             logger.log(Level.WARNING, "Error in createSchema: " + e.getMessage());
         }
         return null;
     }
 
-    public String createDbUser(String userName, String password) {
-        String sql = "CREATE USER " + userName + " WITH PASSWORD '" + password + "'";
+    public String createDbUser(String username, String password) {
+        String sqlCreateDbUser = "CREATE USER " + username + " WITH PASSWORD '" + password + "'";
         try {
-            jdbcTemplate.execute(sql);
-            logger.log(Level.INFO, "User created");
-            return "User created";
+            jdbcTemplate.execute(sqlCreateDbUser);
+            logger.log(Level.INFO, "User " + username + " created");
+            return "User " + username + " created";
         } catch (Exception e) {
             logger.log(Level.WARNING, "Error in createDbUser: " + e.getMessage());
         }
         return null;
+    }
+
+    public String createRole(String roleName, String password) {
+        String sqlCreateRole = "CREATE ROLE " + roleName + " WITH LOGIN PASSWORD '" + password + "'";
+        try {
+            jdbcTemplate.execute(sqlCreateRole);
+            logger.log(Level.INFO, "Role created");
+            return "Role created";
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error in createRole: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public String grantRoleToUser(String roleName, String username) {
+        String sqlGrantRoleToUser = "GRANT " + roleName + " TO " + username;
+        try {
+            jdbcTemplate.execute(sqlGrantRoleToUser);
+            logger.log(Level.INFO, "Role " + roleName + " granted to user " + username);
+            return "Role " + roleName + " granted to user " + username;
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error in grantRoleToUser: " + e.getMessage());
+        }
+        return null;
+    }
+
+
+    public String grantPrivilegeToRole(String schemaName, String roleName, String privilege) {
+        String sqlGrantPrivilege = "GRANT " + privilege + " ON ALL TABLES IN SCHEMA " + schemaName + " TO " + roleName;
+        String sqlGrantDefaultPrivileges = "ALTER DEFAULT PRIVILEGES IN SCHEMA " + schemaName + " GRANT " + privilege + " ON TABLES TO " + roleName;
+        try {
+            jdbcTemplate.execute(sqlGrantPrivilege);
+            jdbcTemplate.execute(sqlGrantDefaultPrivileges);
+            logger.log(Level.INFO, "Privilege " + privilege + " granted to role " + roleName + " on schema " + schemaName);
+            return "Privilege " + privilege + " granted to role " + roleName + " on schema " + schemaName;
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error in grantPrivilegeToRole: " + e.getMessage());
+            return "Error in grantPrivilegeToRole: " + e.getMessage();
+        }
+    }
+
+    public String checkRolePrivilegesOnSchema(String schemaName, String roleName) {
+        String sqlGetPrivileges = "SELECT grantee, table_schema, privilege_type FROM information_schema.role_table_grants WHERE grantee = '" + roleName + "' AND table_schema = '" + schemaName + "'";
+        String sqlCreateTestTable = "CREATE TABLE " + schemaName + ".testtable (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL)";
+        String sqlDropTestTable = "DROP TABLE " + schemaName + ".testtable";
+        try {
+            logger.log(Level.INFO, "Creating test table");
+            jdbcTemplate.execute(sqlCreateTestTable);
+            List<String> privileges = jdbcTemplate.query(sqlGetPrivileges, (rs, rowNum) -> rs.getString("privilege_type"));
+            logger.log(Level.INFO, "Privileges for role " + roleName + " on schema " + schemaName + ": " + privileges);
+            logger.log(Level.INFO, "Dropping test table");
+            jdbcTemplate.execute(sqlDropTestTable);
+            return "Privileges for role " + roleName + " on schema " + schemaName + ": " + privileges;
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "Error in checkRolePrivilegesOnSchema: " + e.getMessage());
+            return "Error in checkRolePrivilegesOnSchema: " + e.getMessage();
+        }
+    }
+
+    public String createSchemaUserAndRole(String schemaName, String username, String password, String roleName) {
+        createSchema(schemaName);
+        createDbUser(username, password);
+        createRole(roleName, password);
+        grantPrivilegeToRole(schemaName, roleName, Privilege.ALL.toString());
+        grantRoleToUser(roleName, username);
+        return checkRolePrivilegesOnSchema(schemaName, roleName);
     }
 }
