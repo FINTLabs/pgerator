bda3928c42e692ca7319c770d2e2cc0743e9f26d
vetlewj <vetlewj@gmail.com>
2022-09-15 15:33:28 +0200

    Update data access tests

--- src/test/groovy/no/fintlabs/operator/pg/service/PostgreSqlDataAccessServiceTest.groovy
+++ src/test/groovy/no/fintlabs/operator/pg/service/PostgreSqlDataAccessServiceTest.groovy
@@ -10,60 +10,75 @@ class PostgreSqlDataAccessServiceTest extends Specification {
     @Autowired
     public PostgreSqlDataAccessService postgreSqlDataAccessService
 
-    def "Creating a database with a given name, the database should be created and return a string saying the database is created"() {
-        when:
+    def "Creating a database with a given name, throws no exceptions"() {
+        given:
         String dbName = "testcreatedb"
-        String dbCreated = postgreSqlDataAccessService.createDb(dbName)
+
+        when:
+        postgreSqlDataAccessService.createDb(dbName)
 
         then:
-        dbCreated == "Database " + dbName + " created"
+        noExceptionThrown()
     }
 
-    def "Creating a schema with a given name should return a string saying the schema is created"() {
-        when:
+    def "Creating a schema with a given name throws no exceptions"() {
+        given:
+        String databaseName = "schematestdb"
         String schemaName = "testcreateschema"
-        String schemaCreated = postgreSqlDataAccessService.createSchema(schemaName)
+
+        when:
+        postgreSqlDataAccessService.createSchema(databaseName, schemaName)
+
         then:
-        schemaCreated == "Schema " + schemaName + " created"
+        noExceptionThrown()
     }
 
-    def "Create a database user with a given username and password"() {
-        when:
+    def "Create a database user with a given username and password throws no exceptions"() {
+        given:
+        String databaseName = "usertestdb"
         String username = "testcreatedbuser"
         String password = "testpw"
-        String userCreated = postgreSqlDataAccessService.createDbUser(username, password)
+
+        when:
+        postgreSqlDataAccessService.createDbUser(databaseName, username, password)
 
         then:
-        userCreated == "User " + username + " created"
+        noExceptionThrown()
     }
 
     def "Create schema and user, and set privileges. Then check if privileges were granted for the user"() {
-        when:
+        given:
+        String databaseName = "privilegetestdb"
         String username = "testgrantprivilegetouser"
         String password = "testpw"
         String schemaName = "testgrantprivilegetouserschema"
-        String userCreated = postgreSqlDataAccessService.createDbUser(username, password)
-        String schemaCreated = postgreSqlDataAccessService.createSchema(schemaName)
         String privilege = "SELECT, INSERT"
-        String privilegeGranted = postgreSqlDataAccessService.grantPrivilegeToUser(schemaName, username, privilege)
-        String privileges = postgreSqlDataAccessService.getUserPrivilegesOnSchema(schemaName, username)
+
+        when:
+        postgreSqlDataAccessService.createDbUser(databaseName, username, password)
+        postgreSqlDataAccessService.createSchema(databaseName, schemaName)
+        postgreSqlDataAccessService.grantPrivilegeToUser(databaseName, schemaName, username, privilege)
+        String privileges = postgreSqlDataAccessService.getUserPrivilegesOnSchema(databaseName, schemaName, username)
 
         then:
-        userCreated == "User " + username + " created"
-        schemaCreated == "Schema " + schemaName + " created"
-        privilegeGranted == "Privilege " + privilege + " granted to " + username + " on schema " + schemaName
-        privileges == "Privileges for " + username + " on schema " + schemaName + ": [INSERT, SELECT]"
+        noExceptionThrown()
+        privileges == "INSERT,SELECT"
     }
 
-    def "Creating a user and schema with privileges should return a message that privileges were set"() {
-        when:
+    def "Creating a user and schema with privileges throws no exceptions and sets privileges"() {
+        given:
+        String databaseName = "privilegetestdb"
         String schemaName = "testschema"
-        String username  ="testuser"
+        String username = "testuser"
         String password = "testpw"
         String privileges = "SELECT, INSERT"
-        String createSchemaUserAndSetPrivileges = postgreSqlDataAccessService.createSchemaUserAndSetPrivileges(schemaName, username, password, privileges)
+
+        when:
+        postgreSqlDataAccessService.createSchemaUserAndSetPrivileges(databaseName, schemaName, username, password, privileges)
+        String getPrivileges = postgreSqlDataAccessService.getUserPrivilegesOnSchema(databaseName, schemaName, username)
 
         then:
-        createSchemaUserAndSetPrivileges == "Privileges for " + username + " on schema " + schemaName + ": [INSERT, SELECT]"
+        noExceptionThrown()
+        getPrivileges == "INSERT,SELECT"
     }
 }
