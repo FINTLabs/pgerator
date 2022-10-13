d34162c36e6f6229f238bfe5b84d6e4973e5db1d
vetlewj <vetlewj@gmail.com>
2022-10-13 12:55:04 +0200

    Update CrdValidator.java

--- src/main/java/no/fintlabs/operator/pg/operator/CrdValidator.java
+++ src/main/java/no/fintlabs/operator/pg/operator/CrdValidator.java
@@ -3,22 +3,27 @@ package no.fintlabs.operator.pg.operator;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 public class CrdValidator {
 
+    public static List<String> MANDATORY_LABELS = Arrays.asList(
+            "app.kubernetes.io/name",
+            "app.kubernetes.io/instance",
+            "app.kubernetes.io/version",
+            "app.kubernetes.io/component",
+            "app.kubernetes.io/part-of",
+            "fintlabs.no/team");
+
     public static void validate(PGSchemaAndUserCRD crd) {
         log.info("Validating CRD");
-        List<String> missingSpecs = new ArrayList<>();
-        if (crd.getSpec().getDatabaseName() == null) {
-            missingSpecs.add("databaseName");
-        }
-        if (crd.getSpec().getSchemaName() == null) {
-            missingSpecs.add("schemaName");
-        }
-        if (!missingSpecs.isEmpty()) {
-            throw new IllegalArgumentException("Missing mandatory specs: " + missingSpecs);
+        List<String> missingLabels = new ArrayList<>(MANDATORY_LABELS);
+        missingLabels.removeAll(crd.getMetadata().getLabels().keySet());
+
+        if (missingLabels.size() > 0 ) {
+            throw new IllegalArgumentException("The following mandatory labels is missing: \n- " + String.join("\n- ", missingLabels));
         }
     }
 }
