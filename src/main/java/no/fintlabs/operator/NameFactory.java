cc0855c51d97333338b5d945ad3e2102f6b47168
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-12 13:57:28 +0200

    We now create schema and give the user rights (#11)

--- src/main/java/no/fintlabs/operator/NameFactory.java
+++ src/main/java/no/fintlabs/operator/NameFactory.java
@@ -7,7 +7,7 @@ public class NameFactory {
     public static String createUsername(PGUserCRD crd) {
         String name = String.format(
                 "%s_%s_%s",
-                crd.getMetadata().getLabels().get("fintlabs.no/org-id").replaceAll("\\.", "-"),
+                crd.getMetadata().getLabels().get("fintlabs.no/org-id").replaceAll("\\.", "_"),
                 crd.getMetadata().getLabels().get("fintlabs.no/team"),
                 RandomStringUtils.randomAlphabetic(7)
         );
@@ -16,7 +16,7 @@ public class NameFactory {
             throw new IllegalArgumentException("Database name to long. It can be max 40 characters.");
         }
 
-        return name;
+        return name.toLowerCase();
     }
 
 
